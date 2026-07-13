71ebfd8b9220ba35152118cb99321ae7237bc404
Pieter Loof <ploo@netcompany.com>
2026-07-13 12:06:49 +0200

    fix: typo in display texts.

--- packages/contracts/src/curve-data-table.ts
+++ packages/contracts/src/curve-data-table.ts
@@ -3,6 +3,6 @@ import { defineContract } from './define-contract.js';
 export const curveDataTableContract = defineContract({
   docs: {
     description:
-      'A custom opiniated data table built on the data table component to reduce the amount of configuration needed to use it.',
+      'A custom opinionated data table built on the data table component to reduce the amount of configuration needed to use it.',
   },
 });
